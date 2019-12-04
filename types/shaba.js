4c92551b861b657da72362e8ebcd10fc53f6a261
Ali Ghaleyan <alighalehban1379@gmail.com>
2019-12-04 12:25:23 +0330

    [feature][add shaba type]

---
+++ types/shaba.js
@@ -0,0 +1,23 @@
+const shaba = class Shaba {
+    constructor() {
+        this.errors = [];
+        this.fails = false;
+        this.label = false;
+    }
+
+    check(value) {
+        //  check value
+        if (value)
+            if (!/^IR\d{24}$/.test(value)) {
+                this.errors.push(" شماره شبا نادرست است.");
+                this.fails = true;
+            }
+        return this;
+    }
+
+    setLabel(label) {
+        this.label = label;
+    }
+};
+
+export default shaba;
